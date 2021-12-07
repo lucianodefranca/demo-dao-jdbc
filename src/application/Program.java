152ea306670ad1e22e67cba70f14385edf1d9872
Luciano de frança <lucianodefranca86gmail.com>
2021-12-07 18:59:01 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,8 @@ public class Program {
 		Department dp = new Department(1, "Books");
 		
 		Seller obj = new Seller(1, "maria", "maria@email", new Date(), 1500.0, dp);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(obj);
 
