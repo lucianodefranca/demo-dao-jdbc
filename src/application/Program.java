ac6fd40c5c22d440fabd5c8e7e35e7202f352334
Luciano de frança <lucianodefranca86gmail.com>
2021-12-07 17:53:13 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,20 @@
 package application;
 
+import java.text.ParseException;
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		Department dp = new Department(1, "Books");
+		
+		Seller obj = new Seller(1, "maria", "maria@email", new Date(), 1500.0, dp);
+		
+		System.out.println(obj);
 
 	}
 
